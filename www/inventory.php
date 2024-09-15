6ac2c95ef5ba8bd61b8d0a4e72f76fcf4e890dee
Jakub Lukac <j.lukac152@gmail.com>
2024-09-15 18:46:38 +0200

    finished removing items from inventory, next is creating new items

--- www/inventory.php
+++ www/inventory.php
@@ -9,7 +9,7 @@ require "header.php";
     <?php require "navbar.php";?>
     <span id="message"></span>
     <div id="inventory">
-        <div class="item-box" onclick="reveal_details(0)">
+        <!-- <div class="item-box" onclick="reveal_details(0)">
             <span class="item-name" id="item-name-0">Example item</span>
             <span class="item-type" id="item-type-0">Example type</span>
             <div class="item-numbers">
@@ -23,17 +23,19 @@ require "header.php";
                 <div class="button" id="increase-charge-0" onclick="increase_charge(0)">+</div>
             </div>
             <span class="item-description" id="item-description-0">example example example example example example example example example example example</span>
-        </div>
+        </div> -->
         <?php
         require "dbconnect.php";
         $sql = "SELECT inventory.id, inventory.name, type.name as type, inventory.amount, inventory.charges, inventory.charges_max, inventory.description 
-            FROM kdnd.inventory INNER JOIN kdnd.type ON inventory.type_id=type.id";
+            FROM kdnd.inventory INNER JOIN kdnd.type ON inventory.type_id=type.id
+            WHERE is_deleted=0";
         if($_SESSION['role'] != 1){
             $sql = $sql." WHERE inventory.character_id=".$_SESSION['cid'];
         }
         $result = $conn->query($sql);
         while($row = $result->fetch_assoc()){
-            echo "<div class='item-box' onclick='reveal_details(".$row['id'].")'>";
+            echo "<div class='item-box' id='item-box-".$row['id']."' onclick='reveal_details(".$row['id'].")'>";
+                echo '<img src="images/remove.png" alt="rmv button" width="50" height="50" class="remove-button" id="remove-button-'.$row['id'].'" onclick="remove_item('.$row['id'].')">';
                 echo "<span class='item-name' id='item-name-".$row['id']."'>".$row['name']."</span>";
                 echo "<span class='item-type' id='item-type-".$row['id']."'>".$row['type']."</span>";
                 echo '<div class="item-numbers">';
@@ -47,8 +49,8 @@ require "header.php";
                     echo '</div>';
                     echo '<div class="button" id="increase-charge-'.$row['id'].'" onclick="increase_charge('.$row['id'].')">+</div>';
                 echo '</div>';
-                echo "<span class='item-description' id='item-description-".$row['id']."' onclick='hide_details(".$row['id'].")'>".$row['description']."</span>";
             echo "</div>";
+            echo "<span class='item-description' id='item-description-".$row['id']."' onclick='hide_details(".$row['id'].")'>".$row['description']."</span>";
         }
         ?>
     </div>
@@ -59,9 +61,9 @@ require "header.php";
 const message = document.getElementById("message");
 var shown_id = 0;
 function reveal_details(id){
-    // if(shown_id != 0){//TODO: put back once the Example item gets removed
+    if(shown_id != 0){//TODO: put back once the Example item gets removed
         document.getElementById("item-description-"+shown_id).style.display = "none";
-    // }
+    }
     document.getElementById("item-description-"+id).style.display = "block";
     shown_id = id;
 }
@@ -77,23 +79,30 @@ function modify_number(id, type, value){
         if(this.readyState == 4 && this.status == 200){
             if(this.responseText == "0"){
                 message.style.display = "block";
-                message.innerHTML = "Successfully changed "+document.getElementById("item-name-"+id).innerHTML + " to " + value;
+                message.innerHTML = "Successfully changed "+document.getElementById("item-name-"+id).innerHTML + "'s " + type + " to " + value;
                 message.style.color = "green";
-                setTimeout(function(){ message.style.display = "none" }, 3000);
-                return 0;
+                setTimeout(function(){ message.style.display = "none" }, 2000);
+                if(type == "is_deleted"){
+                    const removed_item = document.getElementById("item-box-"+id);
+                    removed_item.remove();
+                }
             }
             if(this.responseText == "1"){
                 message.style.display = "block";
                 message.innerHTML = "something went wrong";
                 message.style.color = "red";
                 setTimeout(function(){ message.style.display = "none" }, 3000);
-                return 1;
             }
         }
     };
     request.open("POST", "modify_attempt.php", true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.send(posted_text);
+}
+function remove_item(id){
+    //TODO: make a timeout or something for reverting the deletion
+    modify_number(id, "is_deleted", 1);
+    return;
 }
 function decrease_amount(id){
     const target = document.getElementById("item-amount-"+id);
--- www/login.php
+++ www/login.php
@@ -6,19 +6,19 @@ require "header.php"
 </head>
 <body>
 <div id="login">
+    <span id="message"></span>
     <form action="javascript:void(0);">
         <input type="text" id="cname" name="cname" placeholder="enter character name">
         <input type="password" name="password" id="password" placeholder="enter password">
         <input type="submit" id="login-button" name="login-button" value="Login" onclick="loginAttempt()">
     </form>
-    <span id="message"></span>
 </div>
 </body>
 </html>
 <script>
 const cname_input = document.getElementById("cname");
 const password_input = document.getElementById("password");
-const login_message = document.getElementById("message");
+const message = document.getElementById("message");
 
 function loginAttempt(){
     var request = new XMLHttpRequest();
@@ -26,17 +26,17 @@ function loginAttempt(){
     request.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             if(this.responseText == "0"){
-                login_message.innerHTML = "Successfully logged in";
-                login_message.style.color = "green";
+                message.innerHTML = "Successfully logged in";
+                message.style.color = "green";
                 setTimeout(function(){ window.location.href = "index.php" }, 800);
             }
             if(this.responseText == "1"){
-                login_message.innerHTML = "Character doesn't exist";
-                login_message.style.color = "red";
+                message.innerHTML = "Character doesn't exist";
+                message.style.color = "red";
             }
             if(this.responseText == "2"){
-                login_message.innerHTML = "Incorrect password";
-                login_message.style.color = "red";
+                message.innerHTML = "Incorrect password";
+                message.style.color = "red";
             }
         }
     };
