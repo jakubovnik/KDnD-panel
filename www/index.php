b750e88b8e1d87be9b7bcf9beb0025f5dc110e16
Jakub Lukac <j.lukac152@gmail.com>
2025-02-06 23:46:56 +0100

    images now show up in index.php and can be cycled through (but there is an issue, more in README)

--- www/index.php
+++ www/index.php
@@ -28,13 +28,59 @@ require "php/header.php";
             <span id="character-description"><?php echo $_SESSION['description']?></span>
         </div>
         <div id="character-visual">
-            <span id="character-image-description">example description to nizen</span>
+            
             <!-- TODO: add some kind of description through php -->
-            <img src="images/Nizen.jpg" alt="nizen example" id="character-image">
+            <!-- <img src="images/Nizen.jpg" alt="nizen example" class="character-image" id="character-image"> -->
             <!-- TODO: save images into a database -->
+            <?php
+                $sql = "SELECT id, title, description FROM kdnd.character_image WHERE character_id=".$_SESSION['cid']." ORDER BY id";
+                $result = $conn->query($sql);
+                $image_count = 0;
+                if($result->num_rows <= 0){
+                    echo '<img src="images/Nizen.jpg" alt="nizen example" class="character-image" id="character-image-0">';
+                }else{
+                    while($row = $result->fetch_assoc()){
+                        echo '<div id="character-image-title-'.$image_count.'" class="character-image-title" style="display:none">'.$row['title'].'</div>';
+                        echo '<div id="character-image-description-'.$image_count.'" class="character-image-description" style="display:none">'.$row['description'].'</div>';
+                        echo '<img src="php/get_character_image.php?image_id='.$row['id'].'" onclick="show_next_image()"';
+                        echo ' alt="character image" class="character-image" id="character-image-'.$image_count.'" style="display:none">';
+                        $image_count = $image_count + 1;
+                    }
+                }
+                
+            ?>
         </div>
     </div>
 </div>
 </html>
 <script src="js/default.js"></script>
 <?php if($_SESSION['style'] == "mobile-style.css"){echo '<script src="js/mobile.js"></script>';}?>
+<script>
+
+var last_image_index = 0;
+function set_last_image_index(index){
+    last_image_index = index;
+}
+
+var current_image_index = 0;
+// setTimeout(() => {
+    document.getElementById("character-image-title-0").style.display = "block";
+    document.getElementById("character-image-description-0").style.display = "block";
+    document.getElementById("character-image-0").style.display = "inline-block";
+// }, 1000);
+function show_next_image(){
+    document.getElementById("character-image-title-"+current_image_index).style.display = "none";
+    document.getElementById("character-image-description-"+current_image_index).style.display = "none";
+    document.getElementById("character-image-"+current_image_index).style.display = "none";
+    current_image_index = current_image_index + 1;
+    if(current_image_index >= last_image_index){
+        current_image_index = 0;
+    }
+    document.getElementById("character-image-title-"+current_image_index).style.display = "block";
+    document.getElementById("character-image-description-"+current_image_index).style.display = "block";
+    document.getElementById("character-image-"+current_image_index).style.display = "inline-block";
+}
+</script>
+<?php
+echo "<script>set_last_image_index(".$image_count.");</script>";
+?>
---
+++ www/php/get_character_image.php
@@ -0,0 +1,10 @@
+<?php
+require "dbconnect.php";
+$sql = "SELECT character_image.image, character_image.type FROM kdnd.character_image WHERE id=".$_GET['image_id'];
+$result = $conn->query($sql);
+while($row = $result->fetch_assoc()){
+    header("Content-type: ".$row['type']);
+    echo $row['image'];
+}
+$conn.close();
+?>
