6b527b36fde714c068e72e26d1678fcfae372c1c
Jakub Lukac <j.lukac152@gmail.com>
2025-03-26 20:58:55 +0100

    css for wiki is done for both mobile and desktop, also added a button for showing the lore list (mobile only) and rearanged the html structure of the wiki a bit

--- www/php/get_lore_list.php
+++ www/php/get_lore_list.php
@@ -10,13 +10,20 @@ $sql = "SELECT
         FROM
             `wiki`
         WHERE ".$_POST['type']." LIKE '%".$_POST['search']."%' 
-        ORDER BY `name`;";
+        ORDER BY `tags`, `name`;";
 $result = $conn->query($sql);
 while($row = $result->fetch_assoc()){
-    echo '<div id="lore-item-"'.$row['id'].' class="lore-item">';
-        echo '<a href="?search='.$row['name'].'" class="lore-item-name">'.$row['name'].'</a>';
+    $lore_restricted = "";
+    $lore_name = $row['name'];
+    if($_SESSION['role'] > $row['access']){
+        if(strpos($row['exceptions'], ";".$_SESSION['cname'].";") === FALSE){
+            $lore_restricted = " lore-restricted";
+            $lore_name = "spoiler";
+        }
+    }
+    echo '<div id="lore-item-'.$row['id'].'" class="lore-item">';
+        echo '<a href="?s='.$lore_name.'" class="lore-item-name'.$lore_restricted.'">'.$row['name'].'</a>';
     echo '</div>';
 }
-
 $conn->close();
 ?>
--- www/wiki.php
+++ www/wiki.php
@@ -10,8 +10,8 @@ require "php/header.php";
     <div id="wiki-data">
         <?php
             require "php/dbconnect.php";
-            if(isset($_GET['search'])){
-                $searched_lore = $_GET['search'];
+            if(isset($_GET['s'])){
+                $searched_lore = $_GET['s'];
             }else{
                 $searched_lore = "KDnD";
             }
@@ -20,31 +20,51 @@ require "php/header.php";
             if($result->num_rows > 0){
                 $row = $result->fetch_assoc();
             }
-            $conn->close();
             if(isset($row)){
                 if($_SESSION['role'] == 1 && $_SESSION['edit-mode'] == 1){
-                    echo '<input type="text" id="wiki-name" value="'.$row['name'].'">';
-                    echo "<input type='text' id='wiki-tags' value='".$row['tags']."'>";
-                    echo '<button id="wiki-save-button" onclick="edit_wiki()">save</button>';
+                    echo '<input type="text" id="wiki-name" class="wiki-text-field" value="'.$row['name'].'">';
+                    echo "<input type='text' id='wiki-tags' class='wiki-text-field' value='".$row['tags']."'>";
+                    echo "<select id='wiki-access' class='wiki-text-field'>";
+                        $sql = "SELECT * FROM `role` ORDER BY id";
+                        $result = $conn->query($sql);
+                        while($raw = $result->fetch_assoc()){
+                            echo '<option value="'.$raw['id'].'"';
+                            if($raw['id'] == $row['access']){
+                                echo ' selected';
+                            }
+                            echo '>'.$raw['name'].'</option>';
+                        }
+                    echo "</select>";
+                    echo "<input type='text' id='wiki-exceptions' class='wiki-text-field' value='".$row['exceptions']."'>";
+                    echo '<button id="wiki-save-button" class="wiki-text-field" onclick="edit_wiki()">save</button>';
                 }else{
-                    echo '<span id="wiki-name">'.$row['name'].'</span>';
-                    echo "<span id='wiki-tags'>".$row['tags']."</span>";
+                    echo '<span id="wiki-name" class="wiki-text-field">'.$row['name'].'</span>';
+                    echo "<span id='wiki-tags' class='wiki-text-field' title='".$row['tags']."'>".$row['tags']."</span>";
                 }
             }else{
                 if($_SESSION['role'] == 1 && $_SESSION['edit-mode'] == 1){
-                    echo '<input type="text" id="wiki-name" value="Name">';
-                    echo "<input type='text' id='wiki-tags' value='#unfinished'>";
-                    echo '<button id="wiki-save-button" onclick="edit_wiki()">save</button>';
+                    echo '<input type="text" id="wiki-name" class="wiki-text-field" value="Name">';
+                    echo "<input type='text' id='wiki-tags' class='wiki-text-field' value='#unfinished'>";
+                    echo "<select id='wiki-access' class='wiki-text-field'>";
+                        $sql = "SELECT * FROM `role` ORDER BY id";
+                        $result = $conn->query($sql);
+                        while($raw = $result->fetch_assoc()){
+                            echo '<option value="'.$raw['id'].'"';
+                            if($raw['id'] == "1"){
+                                echo ' selected';
+                            }
+                            echo '>'.$raw['name'].'</option>';
+                        }
+                    echo "</select>";
+                    echo "<input type='text' id='wiki-exceptions' class='wiki-text-field' value=';'>";
+                    echo '<button id="wiki-save-button" class="wiki-text-field" onclick="edit_wiki()">save</button>';
                 }else{
-                    echo '<span id="wiki-name">Not found</span>';
-                    echo "<span id='wiki-tags'>BLANK</span>";
+                    echo '<span id="wiki-name" class="wiki-text-field">Not found</span>';
+                    echo "<span id='wiki-tags' class='wiki-text-field'>#BLANK</span>";
                 }
             }
+            $conn->close();
         ?>
-        <div id="wiki-search-box">
-            <input type="text" placeholder="serach lore" id="search-lore-input">
-            <button id="search-lore-type-button" onclick="switch_button()">name</button>
-        </div>
     </div>
     <div id="wiki">
         <div id="wiki-content">
@@ -59,20 +79,40 @@ require "php/header.php";
                     if($_SESSION['role'] == 1 && $_SESSION['edit-mode'] == 1){
                         echo "<textarea id='wiki-content-edit'></textarea>";
                     }else{
-                        echo "something went wrong (search failed or the lore doesnt exist)";
+                        echo "something went wrong (search failed, the lore doesnt exist or it has spoilers o_o)";
                     }
                 }
             ?>
         </div>
-        <div id="lore-list"></div>
+        <div id="lore-search-button">
+            <img src="images/search.png" alt="mobile search button" id="lore-search-button-image" onclick="toggle_lore_list()">
+        </div>
+        <div id="lore-list-container">
+            <div id="lore-search-box">
+                <input type="text" placeholder="serach lore" id="search-lore-input" class='wiki-text-field'>
+                <button id="search-lore-type-button" class='wiki-text-field' onclick="switch_button()">name</button>
+            </div>
+            <div id="lore-list"></div>
+        </div>
     </div>
 <script src="js/default.js"></script>
 <?php if($_SESSION['style'] == "mobile-style.css"){echo '<script src="js/mobile.js"></script>';}?>
 <script>
 lore_list = document.getElementById("lore-list");
+lore_list_container = document.getElementById("lore-list-container");
 search_input = document.getElementById("search-lore-input");
 search_type_button = document.getElementById("search-lore-type-button");
-
+lore_list_container_visible = 0;
+function toggle_lore_list(){
+    if(lore_list_container_visible == 0){
+        lore_list_container.style.display = "block";
+        lore_list_container_visible = 1;
+        search_input.focus();
+    }else{
+        lore_list_container.style.display = "none";
+        lore_list_container_visible = 0;
+    }
+}
 function fetch_lore_list(){
     var request = new XMLHttpRequest();
     var posted_text = "search=" + search_input.value + "&type=" + search_type_button.innerHTML;
@@ -113,6 +153,8 @@ function edit_wiki(){
     wiki_name = document.getElementById("wiki-name");
     wiki_content = document.getElementById("wiki-content-edit");
     wiki_tags = document.getElementById("wiki-tags");
+    wiki_access = document.getElementById("wiki-access");
+    wiki_exceptions = document.getElementById("wiki-exceptions");
     if(wiki_name.value == ""){
         display_message("please fill in the name", 1);
         return;
@@ -129,8 +171,8 @@ function edit_wiki(){
         wiki_name.value,
         wiki_content.value,
         wiki_tags.value,
-        "1",
-        ""
+        wiki_access.value,
+        wiki_exceptions.value
     );
 }
 function switch_button(){
