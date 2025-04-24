d75c533be31ad99752b139f01d2c8abd5bc2a4de
Jakub Lukac <j.lukac152@gmail.com>
2025-04-24 16:54:43 +0200

    fixed issue when editing a character (constraint errors because of how REPLACE works)

--- www/php/create_character.php
+++ www/php/create_character.php
@@ -1,22 +1,27 @@
 <?php
 session_start();
 require "dbconnect.php";
-$table_id_name = "";
-$table_id_value = "";
-if($_POST['id'] != "-1"){
-    $table_id_name = "`id`, ";
-    $table_id_value = "'".$_POST['id']."', ";
+if($_POST['id'] == "-1"){
+    $sql = "INSERT INTO `character` (`name`, `password`, `age`, `circles`, `description`, `money`, `role_id`) 
+    VALUES
+    ('".$_POST['name']."',
+    '".$_POST['password']."',
+    '".$_POST['age']."',
+    '".$_POST['circles']."',
+    '".$_POST['description']."',
+    '".$_POST['money']."',
+    '".$_POST['role_id']."');";
+}else{
+    $sql = "UPDATE `character` SET ".
+    "name='".$_POST['name']."', ".
+    "password='".$_POST['password']."', ".
+    "age='".$_POST['age']."', ".
+    "circles='".$_POST['circles']."', ".
+    "description='".$_POST['description']."', ".
+    "money='".$_POST['money']."', ".
+    "role_id='".$_POST['role_id']."' ".
+    "WHERE id=".$_POST['id'];
 }
-$sql = "REPLACE INTO `character` (".$table_id_name."`name`, `password`, `age`, `circles`, `description`, `money`, `role_id`) 
-VALUES
-(".$table_id_value."
-'".$_POST['name']."',
-'".$_POST['password']."',
-'".$_POST['age']."',
-'".$_POST['circles']."',
-'".$_POST['description']."',
-'".$_POST['money']."',
-'".$_POST['role_id']."');";
 if($conn->query($sql) === TRUE){
     echo "0";
 }else{
