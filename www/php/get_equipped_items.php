3f9320d25ecc06f1e6f174d04002f5c9bca40383
jakubovnik <j.lukac152@gmail.com>
2026-02-11 19:55:54 +0100

    started on improving the equip system

--- www/php/get_equipped_items.php
+++ www/php/get_equipped_items.php
@@ -4,21 +4,25 @@ if(session_status() === PHP_SESSION_NONE){
 }
 require "dbconnect.php";
 $sql = "SELECT inventory.id, 
-                inventory.name, 
-                inventory.description
-        FROM `inventory` 
+                inventory.name as item_name, 
+		inventory.description,
+		type.name as type_name
+	FROM `inventory` 
+	JOIN
+	    type ON type.id=inventory.type_id
         WHERE is_deleted=0 AND
                 is_equipped=1 AND
                 character_id=".$_SESSION['cid'].
-        " ORDER BY type_id";
+        " ORDER BY inventory.type_id";
 
 $result = $conn->query($sql);
-while($row = $result->fetch_assoc()){
-    echo "<div id='equipped-item-".$row['id']."' class='equipped-item'>";
-        echo $row['name'];
-        echo "<div id='equipped-item-description-".$row['id']."' class='equipped-item-description'>";
-            echo $row['description'];
-        echo "</div>";
-    echo "</div>";
-}
+// while($row = $result->fetch_assoc()){
+//     echo "<div id='equipped-item-".$row['id']."' class='equipped-item'>";
+//         echo $row['item_name'];
+//         echo "<div id='equipped-item-description-".$row['id']."' class='equipped-item-description'>";
+//             echo $row['description'];
+//         echo "</div>";
+//     echo "</div>";
+// }
+
 ?>
